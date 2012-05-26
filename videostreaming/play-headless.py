9bf936a98fabbeac933570b7c49e6f55ab6ce822
Derek Kulinski <takeda@takeda.tk>
2012-05-26 00:59:17 -0700

    Moved Alex's changes to play-headless.py

--- videostreaming/play-headless.py
+++ videostreaming/play-headless.py
@@ -6,18 +6,40 @@ import gst
 import gobject
 
 import sys
+import audio_sink, video_sink
 import audio_src, video_src
 
-if __name__ == '__main__':
+import player
+
+def usage(argv0):
+	print >> sys.stderr, "Usage:"
+	print >> sys.stderr, "for progress logging:"
+	print >> sys.stderr, "%s [videoURI]\n" % argv0
+	print >> sys.stderr, "or for ascii playback:"
+	print >> sys.stderr, "%s [videoURI] ascii" % argv0
+
+def main(args):
 	gobject.threads_init()
 
-	if len(sys.argv) != 2:
-		print >> sys.stderr, "Usage: %s <uri>" % sys.argv[0]
-		sys.exit(1)
+	if len(args) < 2:
+		usage(args[0])
+		return 1
 
-	pipeline = gst.parse_launch("VideoSrc name=source ! progressreport ! fakesink sync=true")
-	src = pipeline.get_by_name('source')
-	src.set_property('location', sys.argv[1])
+	name = player.get_latest_version(args[1])
+	if name is None:
+		print "No content found at %s" % args[1]
+		return 1
+
+	# build pipelines for both realtime progress logging & ascii view
+	PROG = "VideoSrc location=%s/video ! progressreport ! fakesink sync=true" % name
+	ASCII = "VideoSrc location=%s/video ! ffdec_h264 ! aasink" % name
+
+	if len(args) == 3 and args[2] == "ascii":
+		args = ASCII
+	else:
+		args = PROG
+
+	pipeline = gst.parse_launch(args)
 
 	loop = gobject.MainLoop()
 	pipeline.set_state(gst.STATE_PLAYING)
@@ -30,3 +52,6 @@ if __name__ == '__main__':
 
 	pipeline.set_state(gst.STATE_NULL)
 	pipeline.get_state(gst.CLOCK_TIME_NONE)
+
+if __name__ == '__main__':
+	sys.exit(main(sys.argv))
